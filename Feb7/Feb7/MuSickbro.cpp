5ed01dae98863a0ba4e61f2f61c25f7c8a7ddabc
Pr0tag0n1st <665825@dpsk12.net>
2017-02-08 08:21:19 -0700

    slight changes

--- Feb7/Feb7/MuSickbro.cpp
+++ Feb7/Feb7/MuSickbro.cpp
@@ -12,7 +12,7 @@ int main() {
 	sample = al_load_sample("FFVII.wav");
 	display = al_create_display(640, 480);
 	al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
-	al_rest(15.0);
+	al_rest(60.0);
 	al_destroy_display(display);
 	al_destroy_sample(sample);
 }
