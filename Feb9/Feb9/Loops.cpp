a4cedf2ac9dd7bd1d34f25fe6b30f606b8267c46
Pr0tag0n1st <665825@dpsk12.net>
2017-02-09 07:50:35 -0700

    Dory

---
+++ Feb9/Feb9/Loops.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 0; i < 200; i++) {
+		cout << "I shall call him squishy and he shall be mine." << endl;
+	}
+	cout << "" << endl << endl << endl;
+	cout << "From Disney Pixar's 'Finding Nemo'." << endl;
+}
