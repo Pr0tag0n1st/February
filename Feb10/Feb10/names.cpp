72475ecae6815a925a2f2b6847881cd1f8631977
Pr0tag0n1st <665825@dpsk12.net>
2017-02-10 08:28:51 -0700

    nothing new

--- Feb10/Feb10/names.cpp
+++ Feb10/Feb10/names.cpp
@@ -3,7 +3,7 @@ int main() {
 	char string[64];
 	printf("Would you tell me your full name?\n");
 	fgets(string, 64, stdin);
-	printf("Hello, %s!  Nice to meet you!/n", string);
+	printf("Hello, %s!  Nice to meet you!\n", string);
 	return 0;
 
 }
