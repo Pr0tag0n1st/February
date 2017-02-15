27769793a6ee5080fd9b513d477ee5e01512bcd1
Pr0tag0n1st <665825@dpsk12.net>
2017-02-15 07:40:57 -0700

    Loop

--- Feb13/Feb13/Card.cpp
+++ Feb13/Feb13/Card.cpp
@@ -12,7 +12,6 @@ int main() {
 	al_init_font_addon();
 	al_init_acodec_addon();
 	al_reserve_samples(1);
-
 	ALLEGRO_DISPLAY*display = NULL;
 	ALLEGRO_SAMPLE*sample = NULL;
 	ALLEGRO_FONT*font = al_create_builtin_font();
@@ -25,12 +24,6 @@ int main() {
 	al_draw_text(font, ALLEGRO_COLOR(al_map_rgb(0, 0, 0)), 420, 270, 1, "You have the key to my heart!");
 	sample = al_load_sample("Light.wav");
 	al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
-
-
-
-
-
-
 	al_flip_display();
 	al_rest(100.0);
 	al_destroy_display(display);
